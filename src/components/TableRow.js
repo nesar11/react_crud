a0299c6c12b921f29259df1d2ab009fc30de461f
nesar11 <nesar.uddin100@gmail.com>
2022-03-15 23:05:52 +0800

    edit business

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 class TableRow extends Component {
   render() {
@@ -14,7 +16,7 @@ class TableRow extends Component {
             {this.props.obj.business_gst_number}
           </td>
           <td>
-            <button className="btn btn-primary">Edit</button>
+          <Link to={"/edit/"+this.props.obj._id} className="btn btn-primary">Edit</Link>
           </td>
           <td>
             <button className="btn btn-danger">Delete</button>
